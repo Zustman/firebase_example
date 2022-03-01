eebdad9e81a49e26d2a28a2cfc3e59710f3fa93b
Zustman <98294841+Zustman@users.noreply.github.com>
2022-03-01 15:57:51 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  static FirebaseAnalyticsObserver observer =
+      FirebaseAnalyticsObserver(analytics: analytics);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase example',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: FirebaseApp(
+        analytics: analytics,
+        observer: observer,
+      ),
+    );
+  }
+}
+
+class FirebaseApp extends StatefulWidget {
+  const FirebaseApp({Key? key, required this.analytics, required this.observer})
+      : super(key: key);
+  final FirebaseAnalytics analytics;
+  final FirebaseAnalyticsObserver observer;
+
+  @override
+  _FirebaseAppState createState() => _FirebaseAppState(analytics, observer);
+}
+
+class _FirebaseAppState extends State<FirebaseApp> {
+  _FirebaseAppState(this.analytics, this.observer);
+
+  final FirebaseAnalytics analytics;
+  final FirebaseAnalyticsObserver observer;
+  String _message = '';
+
+  void setMessage(String message) {
+    setState(() {
+      _message = message;
+    });
+  }
+
+  Future<void> _sendAnalyticsEvent() async {
+    await analytics.logEvent(
+      name: 'test_event',
+      parameters: <String, dynamic>{
+        'string': 'hello flutter',
+        'int': 100,
+      },
+    );
+    setMessage('Analytics 보내기 성공');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Firebase exampale'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(onPressed: _sendAnalyticsEvent, child: Text('테스트')),
+            Text(
+              _message,
+              style: TextStyle(color: Colors.blueAccent),
+            ),
+          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.tab),
+        onPressed: () {},
+      ),
+    );
+  }
+}
